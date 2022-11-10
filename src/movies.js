3d3ff546e85d232114b076780ac25f241b62a46a
AzizaHalder <azizahalder9@gmail.com>
2022-11-10 18:16:27 +0100

    Iteration 2 done

--- src/movies.js
+++ src/movies.js
@@ -12,10 +12,58 @@ function getAllDirectors(moviesArray) {
 // why doesn't this console log work!!!
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  const dramaFilms = moviesArray.filter((film) => {
+    return film.director === "Steven Spielberg" && film.genre.includes("Drama");
+  });
+
+  return dramaFilms.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) {
+    return 0;
+  } else {
+    const totalScore = movies.reduce(function (total, currentValue) {
+      return total + currentValue.score;
+      const averageRating = totalScore / moviesArray.length.toFixed(2);
+    });
+  }
+  //   const totalScore = moviesArray.reduce(function (total, currentValue) {
+  //     return total + currentValue.score;
+  //   }, 0);
+  //   const averageRating =
+  //     totalScore / moviesArray.length.Math.round(Number * 100) / 100;
+  //   return averageRating;
+
+  //   let myArray = moviesArray.reduce(function (total, current) {
+  //     console.log(total + current.score);
+  //     return total + current.score;
+  //   }, 0);
+  //   console.log(myArray);
+  //   if (moviesArray.score.length === []) {
+  //     return 0;
+  //   } else {
+  //     const totalScore = moviesArray.reduce(function (total, currentValue) {
+  //       return total + currentValue.score;
+  //     }, 0);
+  //     const averageRating = totalScore / moviesArray.length.toFixed(2);
+  //   }
+  //   return averageRating;
+
+  //   const movieScoreAvg = moviesArray.reduce(function (total, current) {
+  //     return total + current;
+  //   }, 0);
+  //   //   return movieScoreAvg;
+  //   const moviesAverage = movieScoreAvg / moviesArray.length;
+  //   return moviesAverage;
+
+  //   const averageRating = moviesArray.reduce(function (total, currentValue) {
+  //     return (total + currentValue.score) / moviesArray.length;
+  //   });
+  //   return averageRating;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
